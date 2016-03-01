dde6c366e74b4518d68a94dca535fa2679bb5943
comRamona <com.ramona@yahoo.com>
2016-03-01 23:51:23 +0000

    finished tests

--- Sem2/2D/CW12D/Inf2dAssignment1/Inf2d.hs
+++ Sem2/2D/CW12D/Inf2dAssignment1/Inf2d.hs
@@ -81,7 +81,7 @@ getUnsignedSymbol symbol
    | isNegated(symbol)=negateSymbol(symbol)
    | otherwise=symbol
 
--- Gets a list of all symbols in for all given sentences
+-- Gets a list of all symbols in all given sentences
 getSymbols :: [Sentence] -> [Symbol]
 getSymbols stmts = remDup(map getUnsignedSymbol (foldl (++) [] (foldl (++) [] stmts)) )  --concatenate symbols from sentences
 
@@ -99,16 +99,17 @@ generateModels (x:[])=[[(x,True)],[(x,False)]]
 generateModels (x:xs) =(map (\a->a++[(x,True)]) (rest))++(map (\a->a++[(x,False)]) (rest)) where rest=generateModels(xs)
 
 
-
 -- This function evaluates the truth value of a propositional sentence using the symbols
 -- assignments in the model.
 -- Check if all the clauses are true
--- A function not in the model evaluates to False, so it wrks with partial assignments too
+-- A symbol not in the model evaluates to False, so it works with partial assignments too
+-- (evaluating to False is a way of disregarding those variables when we are looking for True values)
 pLogicEvaluate :: Sentence -> Model -> Bool
 pLogicEvaluate stmt model = foldl (&&) True (map  (foldl (||) False) values) where values=map (\a->disjunction a model) stmt
 
 -- Helper function which returns a list of assignments for the given list of symbols. A symbol which is not in the model evaluates to False
--- (I avoided throwing an error)
+-- (by doing this I avoided throwing an error.)
+-- Also, this function can be later used to evaluate clauses with partial models.
 disjunction::Clause->Model->[Bool]
 disjunction symbols model=[if isNegated x
                           then not(fromMaybe True (lookupAssignment (getUnsignedSymbol x) model))
@@ -122,22 +123,25 @@ plTrue sentences model =foldl (&&) True ([pLogicEvaluate x model|x<-sentences])
 
 -- This function takes as input a knowledgebase (i.e. a list of propositional sentences),
 -- a query (i.e. a propositional sentence), and a list of symbols.
--- IT recursively enumerates the models of the domain using its symbols to check if there
+-- It recursively enumerates the models of the domain using its symbols to check if there
 -- is a model that satisfies the knowledge base and the query. It returns a list of all such models.
 
--- The task is ambiguous, I followed the handout which states that "the query is satisfied by every model that satisfies the knowledge base".
--- So if there are some models that make the kb true but not the query, return the empty list. This is done by checking for "Nothing" in there
--- output of tge auxiliary function ttCheckAllAux
+-- Check that "the query is satisfied by every model that satisfies the knowledge base".
+-- So if there are some models that make the kb true but not the query, return the empty list.
+-- This is done by checking for "Nothing" in the
+-- output of the auxiliary function ttCheckAllAux (this means there is no entailment).
+-- Also, disregard empty models, which means the KB is false.
 
 ttCheckAll::[Sentence]->Sentence->[Symbol]->[Model]
 ttCheckAll kb query symbols
-  |elem Nothing ans=[]   --return the empty list if there is a case when the mdel satisfies the kb but not the query
+  |elem Nothing ans=[]   --return the empty list if there is a case when the model satisfies the kb but not the query
   |otherwise=map (fromJust) (filter (\x->x/=Just []) ans)   --return the models if there is the case of entailment
  where ans=ttCheckAllAux kb query symbols []
 
---Auxiliary function for ttCheckAll. It returns the list of models that satisfies the knowledge base and the query.
---If a model does not satify the model, it does not matter so we just append [] to the answer
---If a model satisfies the kb but not the query, we return a value of Nothing, so we now the entailment failed
+-- Auxiliary function for ttCheckAll. It returns the list of models that satisfy the knowledge base and the query.
+-- If a model does not satify the model, it does not matter so we just append [] to the answer
+-- If a model satisfies the kb but not the query, we return a value of Nothing, so we know the entailment failed.
+-- (this case is late is checked by ttCheckAll)
 ttCheckAllAux::[Sentence]->Sentence->[Symbol]->Model->[Maybe Model]
 ttCheckAllAux kb query [] model
     | plTrue kb model= if pLogicEvaluate query model then [Just model] else [Nothing]  --returning Nothing means the entailment fails
@@ -149,12 +153,13 @@ ttCheckAllAux kb query symbols model=(ttCheckAllAux kb query rest ((p,True):mode
 
 -- This function determines if a model satisfes both the knowledge base and the query, returning
 -- true or false.
+-- If ttCheck returns any models, the knowledge entails the query.
 ttEntails :: [Sentence] -> Sentence -> Bool
 ttEntails kb query
   |ttCheck==[]=False
   |otherwise=True
-    where symbols=remDup ((getSymbols kb)++(getSymbols [query]))      -- not really necessary to get symbols from the query, if there are not in the kb
-          ttCheck=ttCheckAll kb query symbols                         -- it will return false anyway
+  where symbols=remDup ((getSymbols kb)++(getSymbols [query]))      -- include symbols from the query
+        ttCheck=ttCheckAll kb query symbols
 
 
 -- This function determines if a model satisfes both the knowledge base and the query.
@@ -169,17 +174,18 @@ ttEntailsModels kb query =  ttCheckAll kb query symbols
 -- The early termination function checks if a sentence is true or false even with a
 -- partially completed model.
 
---My implementation returns true if the sentence can be evaluated in any way
+-- returns true if the sentence can be evaluated in any way
 
 earlyTerminate :: Sentence -> Model -> Bool
 earlyTerminate sentence model
  | elem [] partials = False                                                        --return False if any of the clauses cannot be evaluated
  | otherwise=(foldl (&&) True (map (\x->elem (Just True) x) partials)) ||          --check all clauses evaluate to true
                                                                                    --or check if there is a clause that can be evaluated to False
+                                                                                   --(there are no True or unassigned symbols)
              (foldl (||) False (map (\x->not(elem (Just True) x)&&not(elem Nothing x)) partials))
  where partials=getPartials sentence model
 
--- get a list of assignment for a sentence and a partial model
+-- helper function to get a list of assignments for a sentence and a partial model
 getPartials :: Sentence -> Model -> [[Maybe Bool]]
 getPartials sentence model=[map (\x-> lookupAssignment2 x model) clause |clause <- sentence]
 
@@ -190,15 +196,19 @@ getPartials sentence model=[map (\x-> lookupAssignment2 x model) clause |clause 
 -- symbol. If no pure symbol is found, it should return Nothing
 findPureSymbol :: [Symbol] -> [Clause] -> Model -> Maybe (Symbol, Bool)
 findPureSymbol [] clauses model = Nothing
-findPureSymbol (s:sym) clauses model=if pure /= Nothing  then Just (getUnsignedSymbol s,fromJust pure) else findPureSymbol sym clauses model
-                                    where pure=isPure s clauses model
+findPureSymbol (s:sym) clauses model=if pure /= Nothing
+                                     then Just (getUnsignedSymbol s,fromJust pure)
+                                     else findPureSymbol sym clauses model
+                                     where pure=isPure s clauses model
 
+-- helper function which checks whether a symbol is pure by searching through all its occurences
 isPure::Symbol->[Clause]->Model->Maybe Bool
 isPure s clauses model
  | nrNeg==0=Just True               -- if number of occurences of the negated symbol is 0, assign it to true (it always appear positive)
  | nrPos==0=Just False
  | otherwise=Nothing
- --count number ofpositive and negative occurences, disregarding clauses than can already be evaluated to true
+ --count number of positive and negative occurences, disregarding clauses than can already be evaluated to true
+ --note that the disjunction function assigns false to unassigned variables
  where nrPos=sum[ length (filter (\x->x==s) clause)|clause<-clauses, not (elem True (disjunction clause model)) ]
        neg=negateSymbol s
        nrNeg=sum[ length(filter (\x->x==neg) clause)|clause<-clauses, not (elem True (disjunction clause model)) ]
@@ -212,9 +222,10 @@ findUnitClause :: [Clause] -> Model -> Maybe (Symbol, Bool)
 findUnitClause clauses model
     |all/=[]=let x=head(all) in if (isNegated x) then Just(getUnsignedSymbol x,False) else Just(x,True)
     |otherwise=Nothing
-   where all=[fromJust x|clause<-clauses, let x=allButOne clause model, x/=Nothing]
+    --create a list of unit clauses
+    where all=[fromJust x|clause<-clauses, let x=allButOne clause model, x/=Nothing]
 
---helper function which checks whether all symbols but one in a claue are assigned to False
+--helper function which checks whether all symbols but one in a clause are assigned to False
 --and returns that symbol if this is the case
 allButOne::Clause->Model->Maybe Symbol
 allButOne clause model
@@ -231,7 +242,9 @@ allButOne clause model
 dpll :: [Clause] -> [Symbol] -> Bool
 dpll clauses symbols = dpllAux clauses symbols []
 
---helper function for dpll to keep track of models
+-- helper function for dpll to keep track of models
+-- If earlyTerminate returns True, it is safe to use pLogicEvaluate even with a partial model
+-- NOte: pLogicEvaluate assigns False to unassigned symbols
 dpllAux :: [Clause] -> [Symbol] -> Model -> Bool
 dpllAux clauses symbols model
     | earlyTerminate clauses model=pLogicEvaluate clauses model
@@ -260,26 +273,26 @@ dpllSatisfiable clauses =dpll clauses (getSymbols [clauses])
 -- logic), and a query sentence. Both items should have their clauses in CNF representation
 -- and should be assigned to the following respectively:
 evalKB :: [Sentence]
-evalKB = [[["-a","b","c"],["a","c","d"],["a","c","-d"],["a","-c","d"],["a","-c","-d"],
-   ["-b","-c","d"],["-a","b","-c"],["-a","-b","c"],["S11"],["-P11"],["-S11","W12","W11","W21"],["-W12","S11"],["-W11","S11"],
-              ["-W21","S11"],["-W12","-P12"],["-W21","-P21"],["-W11","-W21"],["-W11","-W12"]],[["e","f"],["-e"],["e","m"]]]
+evalKB = [[["-a","b","c"],["a","c","d"],["a","c","-d"],["a","-c","d"],["a","-c","-d"],["-b","-c","d"],
+          ["-a","b","-c"],["-a","-b","c"],["S11"],["-P11"],["-S11","W12","W11","W21"],["-W12","S11"],
+          ["-W11","S11"],["-W21","S11"],["-W12","-P12"],["-W21","-P21"],["-W11","-W21"],["-W11","-W12"]],
+          [["e","f"],["-e"],["e","m"],["-2,-1"]],[["a,1"]]]
 
 evalQuery :: Sentence
-evalQuery = [["S11"]
+evalQuery = [["S11"]]
 
---for dpll evaluation
-sat=concat evalKB
+-- for dpll evaluation
+-- Use refutation to check entailment (check a |= b by checking the unsatisfiability of a || not b)
+-- If ttEntails return True (there is entailment), dpllSatisable should return False.
+sat :: Sentence
+sat=(concat evalKB)++[["-S11"]]
 
 -- RUNTIMES
 -- Enter the average runtimes of the ttEntails and dpllSatisable functions respectively
 
--- Results from nunning various tests:
--- dpllSatisfiable: 0.02 sec ; 0.04; 0.05; 0.04; 0.05
--- ttEntails: 0.09 sec ; 0.32; 0.35; 0.76 ; 0.81
-
 runtimeTtEntails :: Double
-runtimeTtEntails = 0.04
+runtimeTtEntails = 0.35
 
 
 runtimeDpll :: Double
-runtimeDpll = 0.75
+runtimeDpll = 3.12
