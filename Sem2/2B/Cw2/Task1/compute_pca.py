93832d4ef24116f9a8d0032898066c179bbd8272
comRamona <com.ramona@yahoo.com>
2016-03-17 18:26:48 +0000

    finished eigenvectors

--- Sem2/2B/Cw2/Task1/compute_pca.py
+++ Sem2/2B/Cw2/Task1/compute_pca.py
@@ -6,9 +6,20 @@ def compute_pca(x):
     s=(1.0/N)*s
     s=x-s
     st=np.matrix.transpose(s)
-    prod=st*s
+    prod=np.dot(st,s)
+    #covariance matrix
     covariance=(1.0/N)*prod
-    return covariance
+    (EVals,EVecs)=np.linalg.eig(covariance)
+    ev=np.argsort(EVals)
+    ev=ev[::-1]
+    EVecs=EVecs[:,ev]
+    for i in range(0,D):
+        if EVecs[0,i]<0:
+            EVecs[:,i]*=-1
+    return (EVecs,EVals)
 
 data = scipy.io.loadmat('svhn.mat')
 a=data['train_features']
+(vec,val)=compute_pca(a)
+np.savetxt('vec.out', vec, delimiter=',')
+np.savetxt('val.out', val, delimiter=',')
